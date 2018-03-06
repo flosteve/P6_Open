50446c6cec2b1fe92f0d68e9b27b50e3f999249d
flostevebron <eravillesteve@gmail.com>
2018-03-06 19:20:20 +0100

    Install FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EravilleSteve
+ * Date: 06/03/2018
+ * Time: 12:39
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+}
